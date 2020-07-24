d39b0dd08229b56dca165966ab9c1bb7a9712e7e
miracle-halation <miraclehalation.9710@gmail.com>
2020-07-24 14:22:39 +0900

    既読機能のコントローラー側を実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,9 +13,8 @@ class PostsController < ApplicationController
 		if post.checked then
 			post.update(checked: false)
 		else
-			post.update(checked:  true)
+			post.update(checked: true)
 		end
-
 		item = Post.find(params[:id])
 		render json: {post: item}
 	end
